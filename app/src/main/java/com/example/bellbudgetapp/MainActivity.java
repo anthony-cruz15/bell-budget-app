265bc2880df0467de475c4531e2e88ada15232da
antho.cr0783 <antho.cr0783@vusd.us>
2021-11-15 15:24:34 -0800

    11/15 created tabLayout and Fragments

--- app/src/main/java/com/example/bellbudgetapp/MainActivity.java
+++ app/src/main/java/com/example/bellbudgetapp/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.bellbudgetapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 pager;
+    TabLayout tabLayout;
+    SlideAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pager = findViewById(R.id.pager);
+        tabLayout = findViewById(R.id.tabLayout);
+        adapter = new SlideAdapter(this);
+        pager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, pager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                if(position == 0) tab.setText("Breakdown");
+                else if(position == 1) tab.setText("Bells");
+                else tab.setText("Miles");
+            }
+        }).attach();
+    }
+
+    private class SlideAdapter extends FragmentStateAdapter {
+
+        public SlideAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            if(position == 0) return new BreakdownFragment();
+            else if(position == 1) return new BellItemFragment();
+            else return new MileItemFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
     }
 }
