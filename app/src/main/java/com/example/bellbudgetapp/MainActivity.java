141f6df78333c96fb5c88fd00261ea136b16533f
antho.cr0783 <antho.cr0783@vusd.us>
2021-11-17 15:21:22 -0800

    11/17

--- app/src/main/java/com/example/bellbudgetapp/MainActivity.java
+++ app/src/main/java/com/example/bellbudgetapp/MainActivity.java
@@ -4,12 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     ViewPager2 pager;
     TabLayout tabLayout;
     SlideAdapter adapter;
+    RecyclerView.Adapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
